4fa6ad7b686bd9428adf0329a5c2873702f17644
KWONYHULHEE <54491759+KWONYHULHEE@users.noreply.github.com>
2020-01-07 13:01:27 +0900

    Update 나무자르기.cpp

--- algorithmjobs/이진탐색/나무자르기.cpp
+++ algorithmjobs/이진탐색/나무자르기.cpp
@@ -1,10 +1,13 @@
-#include <stdio.h>
-int arr[1000000];
-int m, n;
+
+#include <cstdio>
+using namespace std;
+
+long long int arr[1000010];
+long long int m, n;
 
 
-bool check(int x) {
-	long long int sum;
+bool check(long long int x) {
+	long long int sum=0;
 
 	for (int i = 0; i < n; i++) {
 		if (arr[i] - x < 0)
@@ -12,29 +15,34 @@ bool check(int x) {
 
 		else sum += arr[i] - x;
 	}
-	if (sum >= m)
+	if (sum < m)
 		return true;
 	else return false;
 }
 
 int main() {
 
+	long long int getmax = 0;
 	//Please Enter Your Code Here
-	scanf("%d %d", &n, &m);
+	scanf("%lld %lld", &n, &m);
 
-	for (int i = 0; i < n; i++)
-		scanf("%d", &arr[i]);
-
-	int s = 1;
-	int e = 1000000000;
-
-	while (s + 1 < e) {
-		int mid = (s + e) / 2;
-		if (check(mid)) s = mid;
-		else e = mid;
+	for (int i = 0; i < n; i++) {
+		scanf("%lld", &arr[i]);
+		if (getmax < arr[i])
+			getmax = arr[i];
 	}
 
-	printf("%d", s);
+	long long int s = 0;
+	long long int e = getmax;
+	long long int mid = 0;
+
+	while (s + 1 < e) {
+		 mid = (s + e) / 2;
+		if (check(mid)) e = mid;
+		else s = mid;
+	}
+
+	printf("%lld\n", s);
 
 	return 0;
 }
