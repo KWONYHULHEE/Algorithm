e9bf7a6b3f532a7e5c7e0b48a14a91eb3ff3cb65
KWONYHULHEE <54491759+KWONYHULHEE@users.noreply.github.com>
2020-01-16 17:28:59 +0900

    Add files via upload

--- 특강/BOJ7579앱.cpp
+++ 특강/BOJ7579앱.cpp
@@ -29,11 +29,15 @@ int main() {
 
 
 	for (int i = 1; i <= N; i++) {
-
+		
 		for (int c = cost_sum; c >= cost[i]; c--) {
 
+	
 			dp[c] = max(dp[c], dp[c - cost[i]] + mem[i]);
+
+			//c - cost[i] 에서 cost[i]가 큰값이여야 작은 dp부터 시작하는거
 			//새로운 원소를 넣으며 업데이트 할때는 뒤에서 부터 업데이트해야 겹치지 않을 수 있다!
+
 
 		}
 
@@ -57,28 +61,4 @@ int main() {
 
 	return 0;
 
-}
-
-
-#include <stdio.h>
-#include <algorithm>
-using namespace std;
-
-int a[101], b[101], n, m, d[10001], sum, i, j;
-
-int main() {
-	scanf("%d%d", &n, &m);
-	for (i = 0; i < n; i++) scanf("%d", &a[i]);
-	for (i = 0; i < n; i++) scanf("%d", &b[i]), sum += b[i];
-
-
-	for (i = 0; i < n; i++) {
-		for (j = sum; j >= b[i]; j--) {
-			d[j] = max(d[j], d[j - b[i]] + a[i]);
-		}
-	}
-
-	for (i = 0; i < sum && d[i] < m; i++);
-	printf("%d\n", i);
-	return 0;
 }
