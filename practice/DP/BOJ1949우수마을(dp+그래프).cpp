c6c00054b720ecaeceadfe380b070dda5824f73a
KWONYHULHEE <54491759+KWONYHULHEE@users.noreply.github.com>
2020-01-31 16:44:23 +0900

    u

--- practice/DP/BOJ1949우수마을(dp+그래프).cpp
+++ practice/DP/BOJ1949우수마을(dp+그래프).cpp
@@ -6,38 +6,54 @@ using namespace std;
 
 int n, m;
 vector <int> arr[10010];
+vector <int> tree[10010];
+vector <int> result;
 int dp[10010][2];
-int people[10010];
+int value[10010];
 bool check[10010];
 
+void dfs(int node) {
+
+	check[node] = true;
+
+	for (int i = 0; i < arr[node].size(); i++) {
+		int next = arr[node][i];
+
+		if (!check[next]) {
+			tree[node].push_back(next);
+			dfs(next);
+		}
+
+	}
+
+}
 int solve(int index, int flag) {
 
 	int& ret = dp[index][flag];
 	if (ret != -1) return ret;
 
-	ret = 0;
-	check[index] = true;
+	if (flag) 
+	{
+		ret = value[index];
+		for (int i = 0; i < tree[index].size(); i++)
+		{
 
-
-	for (int i = 0; i < arr[index].size(); i++) {
-		int next = arr[index][i];
-		if (!check[next]) {
-			if (flag == 1) { //우수마을이면
-				ret += solve(next, 0);
-			}
-
-			else { //우수마을이 아니면
-				ret += max(solve(next, 1) + people[next], solve(next, 0));
-			}
-
+			int next = tree[index][i];
+			ret += solve(next, 0);
 		}
 	}
-
-	check[index] = false;
+	else  
+	{
+		ret = 0; 
+		for (int i = 0; i < tree[index].size(); i++)
+		{
+			int next = tree[index][i];
+			ret += max(solve(next, 1), solve(next, 0)); 
+		}
+	}
 	return ret;
 
 }
-
 
 
 int main() {
@@ -47,19 +63,24 @@ int main() {
 
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> people[i];
+		cin >> value[i];
 	}
 
-	for (int i = 1; i <n; i++) {
+	for (int i = 1; i < n; i++) {
 		int a, b;
 		cin >> a >> b;
 		arr[a].push_back(b);
 		arr[b].push_back(a);
 	}
-
+	dfs(1);
 	memset(dp, -1, sizeof(dp));
-	memset(check, false, sizeof(check));
 
-	cout << max(solve(1, 1)+people[1], solve(1, 0));
+
+	cout << max(solve(1, 1), solve(1, 0)) << "\n";
+
+
+
+
+
 	return 0;
 }
